0373f26e4e2c47c19ab245226e66c63346481867
Nadja Tur <nadjatur1999@gmail.com>
2023-02-09 23:05:48 +0100

    styling elements

--- main.js
+++ main.js
@@ -47,7 +47,7 @@ function myFunction() {
        else cssmenu.addClass('dropdown');
        if (settings.sticky === true) cssmenu.css('position', 'fixed');
     resizeFix = function() {
-      var mediasize = 1000;
+      var mediasize = 500;
          if ($( window ).width() > mediasize) {
            cssmenu.find('ul').show();
          }
